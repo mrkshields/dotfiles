53f27d0fb4c34ec828711aa9586754899fbde840
Mark Shields <laebshade@gmail.com>
2017-05-09 23:56:27 -0400

    Automatic tmux start in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,17 @@
+# PATHS
+set PATH $PATH $HOME/Library/Python/2.7/bin
+
 # Powerline config
 set fish_function_path $fish_function_path "$HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/fish"
 powerline-setup
+
+# Tmux/Powerline setup
+if command -v tmux >/dev/null
+  if [ $TERM != 'screen' ]
+    if [ -z $TMUX ]
+      exec tmux
+    end
+  end
+end
+
+powerline-config tmux setup
