b4b3d61b5fb092d5a84460470c3232b7ad213e79
Mark Shields <laebshade@gmail.com>
2017-05-10 00:46:39 -0400

    pretty much complete fish config for daily use

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
-# PATHS
-set PATH $PATH $HOME/Library/Python/2.7/bin
+# Environment variables
+set -x PATH $PATH $HOME/Library/Python/2.7/bin
+set -x EDITOR 'vim'
+set -x FIGNORE '*.pyc'
+set -x PYTHONDONTWRITEBYTECODE 'very_yes'
+
+# Aliases
+alias s 'ssh'
 
 # Powerline config
 set fish_function_path $fish_function_path "$HOME/Library/Python/2.7/lib/python/site-packages/powerline/bindings/fish"
@@ -15,3 +21,44 @@ if command -v tmux >/dev/null
 end
 
 powerline-config tmux setup
+
+# Functions
+function work --argument-names 'target_workdir'
+  if [ $target_workdir = 'source' ]
+    cd $HOME/workspace/source/$SRC
+  else
+    cd $HOME/workspace/$target_workdir
+  end
+
+  if git rev-parse --abbrev-ref HEAD > /dev/null 2>&1
+    if [ (git rev-parse --show-toplevel | awk -F'/' '{print $NF}') = 'source' ]
+      get_src
+      get_tests
+    end
+  end
+end
+
+function src --argument-names 'src_path'
+  if count $src_path > /dev/null
+    set -U SRC $src_path
+  else
+    set -U SRC (echo $PWD | awk -F'/source/' '{print $NF}')
+  end
+end
+
+function tests --argument-names 'tests_path'
+  if count $tests_path > /dev/null
+    set -U SRC $src_path
+    set -U TESTS $tests_path
+  else
+    set -U TESTS (echo $PWD | awk -F'/source/' '{print $NF}' | sed 's/src/tests/')
+  end
+end
+
+function get_src
+  echo -e "SRC:\t$SRC"
+end
+
+function get_tests
+  echo -e "TESTS:\t$TESTS"
+end
