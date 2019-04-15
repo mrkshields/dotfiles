c354cecdc795541f9d68037aaeae70fd871e3c75
Mark Shields <mshields@twitter.com>
2019-04-15 09:55:11 -0400

    update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,31 +3,26 @@ set -l configdir ~/.config
 
 # because fish complains if a path doesn't exist
 
-for path in $HOME/bin /opt/local/bin /opt/twitter_mde/bin /opt/twitter/bin $HOME/Library/Python/2.7/bin $HOME/Library/Python/3.6/bin $HOME/.local/bin /opt/twitter/opt/coreutils/libexec/gnubin /usr/local/opt/coreutils/libexec/gnubin /opt/local/Library/Frameworks/Python.framework/Versions/3.7/bin $HOME/Library/Python/3.7/bin
+for path in $HOME/bin $HOME/go/bin /opt/local/bin /opt/twitter_mde/bin $HOME/Library/Python/2.7/bin $HOME/Library/Python/3.6/bin $HOME/.local/bin /usr/local/opt/coreutils/libexec/gnubin /opt/local/Library/Frameworks/Python.framework/Versions/3.7/bin $HOME/Library/Python/3.7/bin $HOME/workspace/source $HOME/workspace/source/bin
   if test -d $path
     set -x PATH $path $PATH
   end
 end
 
-if test -d /opt/twitter/opt/coreutils/libexec/gnuman
-  set -x MANPATH /opt/twitter/opt/coreutils/libexec/gnuman $MANPATH
-end
+set -x GOPATH $HOME/go
 
 # Powerline config
 if status is-interactive
   set fish_function_path $fish_function_path "$HOME/Library/Python/3.6/lib/python/site-packages/powerline/bindings/fish" "$HOME/.local/lib/python3.6/site-packages/powerline/bindings/fish"
   powerline-setup
-  fzf_key_bindings
-  source (jump shell | psub)
+  #fzf_key_bindings
+  #source (jump shell | psub)
 end
 
 # Fundle plugin installs
 if functions fundle > /dev/null 2>&1
-  #fundle plugin 'oh-my-fish/plugin-grc'
-  fundle plugin 'acomagu/fish-async-prompt'
+  #fundle plugin 'acomagu/fish-async-prompt'
   fundle plugin 'edc/bass'
-  fundle plugin 'fisherman/get'
-  fundle plugin 'fisherman/spin'
   fundle plugin 'oh-my-fish/plugin-bang-bang'
   fundle plugin 'oh-my-fish/plugin-expand'
   fundle plugin 'oh-my-fish/theme-bobthefish'  # won't work without installing ohmyfish framework then using omf to install
@@ -52,14 +47,13 @@ set -x SBT_PROXY_REPO 'http://artifactory.local.twitter.com/repo/'
 # Aliases
 alias s ssh
 alias stripcolor "perl -MTerm::ANSIColor=colorstrip -ne 'print colorstrip(\$_)'"
-alias find gfind
+#alias find gfind
+alias pamm $HOME/workspace/source/ammonite/repl
 #functions -e ls
 
-#if test -x /opt/twitter/bin/vim; alias vim /opt/twitter/bin/vim; end
-#if test -x /opt/twitter_mde/bin/svn; alias svn /opt/twitter_mde/bin/svn; end
-#if test -x /opt/twitter_mde/bin/git; alias git /opt/twitter_mde/bin/git; end
 if which ggrep > /dev/null; alias grep ggrep; end
 alias git-tl "git rev-parse --show-toplevel"
+alias pip "python3.7 -m pip"
 
 
 source $configdir/fish/tmux.fish
@@ -85,18 +79,18 @@ end
 
 function work --argument-names 'target_workdir'
   if count $target_workdir > /dev/null
-    switch $target_workdir
-      case source src
-        cd $HOME/workspace/source/$SRC
-        #set -x PANTS_CONFIG_OVERRIDE $HOME/workspace/source/pants.ini.daemon
-        get_src
-      case tests
-        cd $HOME/workspace/source/$TESTS
-        #set -x PANTS_CONFIG_OVERRIDE $HOME/workspace/source/pants.ini.daemon
-        get_tests
-      case '*'
+#    switch $target_workdir
+#      case source src
+#        cd $HOME/workspace/source/$SRC
+#        #set -x PANTS_CONFIG_OVERRIDE $HOME/workspace/source/pants.ini.daemon
+#        get_src
+#      case tests
+#        cd $HOME/workspace/source/$TESTS
+#        #set -x PANTS_CONFIG_OVERRIDE $HOME/workspace/source/pants.ini.daemon
+#        get_tests
+#      case '*'
         cd $HOME/workspace/$target_workdir
-    end
+#    end
   else
     cd $HOME/workspace
   end
@@ -121,9 +115,9 @@ end
 
 function setup
   src
-  tests
+  #tests
   get_src
-  get_tests
+  #get_tests
 end
 
 function get_src
@@ -132,6 +126,13 @@ end
 
 function get_tests
   echo $TESTS
+end
+
+function projdir --argument-names 'name'
+  if count $name > /dev/null
+    mkdir -pv $name
+    touch $name/PROJECT
+  end
 end
 
 function gc --argument-names 'repo'
