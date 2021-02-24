899237fa340a359a82c14e621dbb9cd992852fe4
Mark Shields <laebshade@gmail.com>
2021-02-24 13:48:26 -0500

    add fish kubectl completion plugin

--- fish/.config/fish/fundle.fish
+++ fish/.config/fish/fundle.fish
@@ -3,6 +3,7 @@ if functions fundle > /dev/null 2>&1
   fundle plugin 'Markcial/upto'
   fundle plugin 'danhper/fish-ssh-agent'
   fundle plugin 'edc/bass'
+  fundle plugin 'evanlucas/fish-kubectl-completions'
   fundle plugin 'fisherman/fzy'
   fundle plugin 'fisherman/spin'
   fundle plugin 'jethrokuan/z'
