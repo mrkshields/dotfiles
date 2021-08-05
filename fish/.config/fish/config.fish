cff1509b7c5fd3d9843ff7694a3151f0313dc8bc
Mark Shields <mark.shields@greenlight.me>
2021-08-05 09:31:06 -0400

    cleanup old bird/bt/macports fish/tmux settings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,9 +7,6 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.npm-global/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/go/bin
-fish_add_path $HOME/macports/bin
-fish_add_path $HOME/macports/sbin
-fish_add_path $HOME/Library/Python/3.7/bin
 fish_add_path /snap/bin
 fish_add_path $HOME/.local/go/bin
 fish_add_path $HOME/.krew
@@ -18,26 +15,15 @@ fish_add_path /usr/local/opt/mysql@5.7/bin
 
 set -x GOPATH $HOME/go
 
-# direnv
-#direnv hook fish | source
-
 # Powerline config
 if status is-interactive
-  for path in $HOME/.local/lib/python3.7/site-packages/powerline/bindings/fish $HOME/macports/Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/powerline/bindings/fish /usr/local/lib/python3.9/site-packages/powerline/bindings/fish
-    if test -d $path
-      set fish_function_path $fish_function_path $path
-      powerline-setup
-    end
-  end
+    set fish_function_path $fish_function_path /usr/local/lib/python3.9/site-packages/powerline/bindings/fish
+    powerline-setup
 end
 
 source $configdir/fundle.fish
 
-# Fish config
-#
 set -g async_prompt_inherit_variables all
-#set -g theme_display_git_dirty no
-#set -g theme_display_git_untracked no
 
 
 # Environment variables
@@ -56,19 +42,13 @@ alias k kubectl
 alias s ssh
 alias m mosh
 alias stripcolor "perl -MTerm::ANSIColor=colorstrip -ne 'print colorstrip(\$_)'"
-alias pamm $HOME/workspace/source/ammonite/repl
 if which ggrep > /dev/null; alias grep ggrep; end
 if which gfind > /dev/null; alias find gfind; end
 if which gls > /dev/null; alias ls gls; end
 if which cpair > /dev/null; alias c cpair; end
 if which tmux > /dev/null; alias t tmux; end
 alias git-tl "git rev-parse --show-toplevel"
-alias pip "python3 -m pip"
 
-
-#source $configdir/tmux.fish
-#powerline-config tmux setup
-#source $configdir/flux.fish
 
 function git-master
   git remote show origin | awk '/HEAD branch:/{printf $NF}'
@@ -78,14 +58,6 @@ function get-shannara
   get-passwd-from-tag-per-session shannara marks | wl-copy
 end
 
-function get-ldap
-  get-passwd-from-tag-per-session ldap braintree | pbcopy
-end
-
-function get-corp
-  get-passwd-from-tag-per-session corp braintree | pbcopy
-end
-
 function unzip-to --argument-names 'file' 'parentdir'
 	set dir (echo $file | awk -F'.zip' '{print $1}')
 	unzip $file -d $parentdir/$dir
@@ -93,34 +65,6 @@ end
 
 function get-passwd-from-tag-per-session --argument-names 'tags' 'session'
   eval (op signin --session $session); and op list items --tags $tags | op get item --fields password -
-end
-
-function cpair-tmux --argument-names 'account'
-  set query (tmux list-windows -F '#{window_active} #W' | awk '/^1/{printf $NF}')
-  cpair-select $account $query
-end
-
-function cpair-window --argument-names 'account'
-  set query (tmux list-windows -F '#{window_active} #W' | awk '/^1/{printf $NF}')
-  cpair-select $account $query
-end
-
-function cpair-select --argument-names 'account' 'query'
-  if count $query > /dev/null
-    set selected (env CPAIR_ACCOUNT=$account cpair list | fzf --header-lines=1 --query $query)
-  else
-    set selected (env CPAIR_ACCOUNT=$account cpair list | fzf --header-lines=1)
-  end
-  echo $selected
-  env CPAIR_ACCOUNT=$account cpair ssh -p (echo $selected | awk '{printf $1}')
-end
-
-function bt --argument-names 'target_workdir'
-  if count $target_workdir > /dev/null
-    cd $HOME/bt/$target_workdir
-  else
-    cd $HOME/bt
-  end
 end
 
 function work --argument-names 'target_workdir'
@@ -134,8 +78,3 @@ end
 function ipmitool
   /opt/local/bin/ipmitool -I lanplus -U root -P root -H $argv
 end
-
-# Extraterm extra integration
-#if test -f $configdir/extraterm/setup_extraterm_fish.fish
-#  source $configdir/extraterm/setup_extraterm_fish.fish
-#end
