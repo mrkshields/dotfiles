2e98da61cd20f8d38fbb93cf98743b7eac3fc9ce
Mark Shields <mark.shields@greenlight.me>
2021-08-09 15:39:59 -0400

    cleaner keychain-environment-variable

--- fish/.config/fish/keychain-environment-variables.fish
+++ fish/.config/fish/keychain-environment-variables.fish
@@ -2,20 +2,18 @@
 ### Adapted from https://www.netmeister.org/blog/keychain-passwords.html ###
 
 # Use: keychain-environment-variable SECRET_ENV_VAR
-function keychain-environment-variable
-  security find-generic-password -w -a $USER -D "environment variable" -s $argv
+function keychain-environment-variable --argument-name env_name
+  test -z "$env_name"; and echo >&2 "Missing environment variable name"; and return 1
+  security find-generic-password -w -a $USER -D "environment variable" -s $env_name
 end
 
 # Use: set-keychain-environment-variable SECRET_ENV_VAR
 #   provide: super_secret_key_abc123
 function set-keychain-environment-variable --argument-name env_name
-  
-  test -n "$env_name"; or echo "Missing environment variable name"
-  read -ps "Enter Value for $env_name: " secret
-  if test -n $1
-    if test -n $secret
-      security add-generic-password -U -a $USER -D "environment variable" -s $env_name -w $secret
-    end
+  test -z "$env_name"; and echo >&2 "Missing environment variable name"; and return 1
+  read -sp "Enter Value for $env_name: " secret
+  if test -n $secret
+    security add-generic-password -U -a $USER -D "environment variable" -s $env_name -w $secret
   end
   return 1
 end
