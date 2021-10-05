3911402a7f1de02a0046a25ea54fa518c3976dc8
Mark Shields <mark.shields@greenlight.me>
2021-10-05 09:12:37 -0400

    add go path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,6 +11,7 @@ fish_add_path /snap/bin
 fish_add_path $HOME/.local/go/bin
 fish_add_path $HOME/.krew
 fish_add_path /usr/local/opt/mysql@5.7/bin
+fish_add_path /usr/local/opt/go@1.16/bin
 
 
 set -x GOPATH $HOME/go
