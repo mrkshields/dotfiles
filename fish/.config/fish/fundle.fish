0f48304cb6767cde43e8b93843664486af27932f
Mark Shields <4237425+beejiujitsu@users.noreply.github.com>
2023-09-05 14:44:40 -0400

    latest fundle

--- fish/.config/fish/fundle.fish
+++ fish/.config/fish/fundle.fish
@@ -1,11 +1,12 @@
 # Fundle plugin installs
 if functions fundle > /dev/null 2>&1
+  #  fundle plugin 'fisherman/fzy'
   fundle plugin 'Markcial/upto'
   fundle plugin 'danhper/fish-ssh-agent'
   fundle plugin 'edc/bass'
   fundle plugin 'evanlucas/fish-kubectl-completions'
-  #  fundle plugin 'fisherman/fzy'
   fundle plugin 'fisherman/spin'
+  fundle plugin 'halostatue/fish-direnv'
   fundle plugin 'jethrokuan/z'
   fundle plugin 'joseluisq/gitnow'
   fundle plugin 'laughedelic/pisces'
@@ -13,7 +14,7 @@ if functions fundle > /dev/null 2>&1
   fundle plugin 'oh-my-fish/plugin-expand'
   fundle plugin 'oh-my-fish/theme-bobthefish'  # won't work without installing ohmyfish framework then using omf to install
   fundle plugin 'tuvistavie/fish-fastdir'
-  fundle plugin 'halostatue/fish-direnv'
+  fundle plugin 'z11i/github-copilot-cli.fish'
 
   fundle init
 end
